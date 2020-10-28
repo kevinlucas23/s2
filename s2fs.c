465c2a0e5c6842410bbea0836fb2ecc35a583d2a
kevinlucas23 <54790615+kevinlucas23@users.noreply.github.com>
2020-10-27 22:57:58 -0400

    Part 1 Finally Done

--- s2fs.c
+++ s2fs.c
@@ -16,7 +16,7 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Kevin");
 MODULE_DESCRIPTION("Print the Process Tree of Linux Project 4 part 1");
 
-void processInfo(struct task_struct* t, int level)
+static void view_tree(struct task_struct* current, int level)
 {
 	struct task_struct* task;
 	struct list_head* list;
@@ -27,107 +27,30 @@ void processInfo(struct task_struct* t, int level)
 
 	// print tree to kernel log
 	if (level > 0) {
-		printk(KERN_INFO "%s%s [%d]\n", offset, t->comm, t->pid);
+		printk(KERN_INFO "%s%s [%d]\n", offset, current->comm, current->pid);
 	}
 	else {
-		printk(KERN_INFO "%s%s [%d]\n", offset, t->comm, t->pid);
+		printk(KERN_INFO "%s%s [%d]\n", offset, current->comm, current->pid);
 	}
 
 	// iterate through children of init process
-	// init_task is list_head not a &list_head
-	// book is wrong! use '.' not '->'
-	list_for_each(list, &t->children) {
+	list_for_each(list, &current->children) {
 		task = list_entry(list, struct task_struct, sibling);
-		processInfo(task, level + 2);
+		view_tree(task, level + 2);
 	}
-	//printk("Process: %s[%d], Parent: %s[%d]\n", task -> comm, task -> pid , task -> parent -> comm, task -> parent -> pid);
 	return;
 }
-
-//void memAndFileInfo(void)
-//{
-//	bool found = false;
-//	struct task_struct *task;
-//	struct task_struct *t = current;
-//	struct mm_struct *m;
-//	struct vm_area_struct *v;
-//	struct files_struct *open_files;
-//	struct fdtable *files_table;
-//	struct kstat *ks;
-//	struct path files_path;
-//	unsigned long t_size = 0;
-//	rcu_read_lock();
-//	for_each_process(task)
-//	{
-//		task_lock(task);
-//		int iid = task -> pid;
-//		if(iid == id)
-//		{
-//			t = task;
-//			found = true;
-//		}
-//		task_unlock(task);
-//
-//	}
-//	rcu_read_unlock();
-//	if(found)
-//	{
-//		printk("//////VIRTUAL MEMORY INFORMATION//////\n\n");
-//		v = t -> mm -> mmap;
-//		if(v != NULL)
-//		{
-//			printk("Process: %s[%d]\n",t -> comm, t -> pid);
-//		while(v -> vm_next != NULL)
-//		{
-//			unsigned long size = v -> vm_end - v -> vm_start;
-//			t_size = t_size + size;
-//			printk("Start: 0x%lx, End: 0x%lx, Block Size: 0x%lx\n", v -> vm_start, v -> vm_end, size);
-//			v = v -> vm_next;
-//		}
-//		printk("Total size of virtual space is: 0x%lx\n",t_size);
-//		printk("\n");
-//		printk("//////OPEN FILES INFORMATION//////\n\n");
-//		printk("Process: %s[%d]\n",t -> comm, t -> pid);
-//		int i = 0;
-//		open_files = t -> files;
-//		files_table = files_fdtable(open_files);
-//		char *path;
-//		char *buf = (char*)kmalloc(10000*sizeof(char),GFP_KERNEL);
-//		if(buf == NULL)
-//		{
-//			printk("ERROR\n");
-//			return 1;
-//		}
-//		while(files_table -> fd[i] != NULL)
-//		{
-//			files_path = files_table -> fd[i] -> f_path;
-//			char* name = files_table-> fd[i] -> f_path.dentry -> d_iname;
-//			long long size = i_size_read(files_table-> fd[i] -> f_path.dentry -> d_inode);
-//			path = d_path(&files_path,buf,10000*sizeof(char));
-//			printk("Name: %s, FD: %d, Size: 0x%llx bytes, Path: %s\n", name, i, size , path);
-//			i++;
-//		}
-//		}
-//	}
-//	else
-//	{
-//		printk("ID not found\n");
-//	}
-//
-//}
 
 static int s2fs_init(void)
 {
 	printk("Loading proctree Module...\n");
-	processInfo(&init_task, 0);
-	printk("\n\n");
-	// memAndFileInfo();
+	view_tree(&init_task, 0);
 	return 0;
 }
 
 static void s2fs_exit(void)
 {
-	printk("Done\n");
+	printk("\n\nDone\n\n");
 }
 
 module_init(s2fs_init);
